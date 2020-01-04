0d373feb92093175c272f76916454fc80e609ce0
nehrning <34261164+nehrning@users.noreply.github.com>
2020-01-04 09:54:19 +0900

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -17,8 +17,14 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if(bfire)
-			transform.Translate(dir * Time.deltaTime * speed);
+		if(bfire){
+			//transform.Translate(dir * Time.deltaTime * speed);
+			Vector3 tmp = transform.position;
+			tmp.x = tmp.x + dir.x * Time.deltaTime * speed;
+			tmp.y = tmp.y + dir.y * Time.deltaTime * speed;
+			tmp.z = tmp.z + dir.z * Time.deltaTime * speed;
+			transform.position = tmp;
+		}
     }
 
 	public void SetFirePosition(Transform _pos)
